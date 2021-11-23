24d12768a6a65fa4e4d03da5251f719372101af9
LeninFranco <90914859+LeninFranco@users.noreply.github.com>
2021-11-23 16:37:58 -0600

    Update procesar.php

--- procesar.php
+++ procesar.php
@@ -1,6 +1,52 @@
 <?php
     session_start();
+    include("lib/db.php");
     $datos = $_SESSION['datos'];
-    print_r($datos);
+    $sql = "INSERT INTO alumnos VALUES('$datos[1]','$datos[2]','$datos[3]','$datos[4]','$datos[5]','$datos[6]','$datos[7]','$datos[8]','$datos[9]','$datos[10]','$datos[11]','$datos[12]','$datos[13]','$datos[14]','$datos[15]','$datos[16]','$datos[17]','$datos[18]')";
+    $result = mysqli_query($conn,$sql);
     session_unset();
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>.: Registro Nuevo Ingreso :.</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://bootswatch.com/5/zephyr/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+        <div class="container-fluid">
+            <a href="index.html" class="navbar-brand">ESCOM | IPN</a>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Recuperar informacion</a>
+                </li>
+            </ul>
+            <img src="img/logoESCOMBlanco.png" width="70px" height="60px">
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row pt-4" style="text-align: center;">
+            <?php if (!$result) { ?>
+                <h1>¡Ya tenemos tu registro!</h1>
+                <h3>Espera tu correo electronico</h3>
+                <img src="img/burrito-bad.png" style="width: 200px; height: 250px; display:block; margin:auto;">
+            <?php } ?>
+            <?php if ($result) { ?>
+                <h1>¡Gracias por registrarte!</h1>
+                <h3>Sus datos fueron almacenados correctamente</h3>
+                <h3>Le enviamos un correo electronico con la fecha y sede de su examen diagnostico</h3>
+                <img src="img/burrito-nice.png" style="width: 250px; height: 200px; display:block; margin:auto;">
+            <?php } ?>
+        </div>
+    </div>
+</body>
+<script src="JS/retorno.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>   
+</html>
